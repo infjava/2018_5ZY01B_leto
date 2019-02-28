2e7ca3a274394bd1f1e70c5017fd75d2feebb653
Jan Janech <jan.janech@kst.uniza.sk>
2019-02-28 13:17:09 +0100

    plan do buducna

--- wof1B/src/Wof00.java
+++ wof1B/src/Wof00.java
@@ -5,7 +5,28 @@
  * @version 21.2.2012
  */
 public class Wof00 {
-
+    /*
+    - predmety
+        - inventar
+        - pero
+        - pozvanka na ples
+        - bageta (+HP)
+        - zhnita bageta (-HP)
+        - kluc od miestnosti
+    - npc
+        - skusky (suboj)
+        - ucitel
+        - dialogy
+    - charakteristiky
+        - HP
+        - inteligencia
+        - sila
+        - obratnost
+    - eventy
+    - questy
+        - spravit skusku
+    */
+    
     /**
      * @param args parametre programu
      */
